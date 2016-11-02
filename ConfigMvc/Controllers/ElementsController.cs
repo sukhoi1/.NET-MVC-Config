58361baf44a54d1ce33b5d44a54294480d3c2076
Anton Lygin <lygin@live.com>
2016-11-03 01:23:12 +0300

    v0.015 - nested web.config, example of appSettings auto-merge (when WebConfigurationManager.GetWebApplicationSection(...) method is called);

--- ConfigMvc/Controllers/ElementsController.cs
+++ ConfigMvc/Controllers/ElementsController.cs
@@ -16,15 +16,32 @@ namespace ConfigMvc.Controllers
             return this.Private(); // UK
         }
 
+        public ActionResult Third()
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            var config = WebConfigurationManager.OpenWebConfiguration("~/Views/FolderConfig");
+            foreach (string key in config.AppSettings.Settings.AllKeys) //Note: not directly WebConfigurationManager!
+            {
+                dict.Add(key, $"{key} {config.AppSettings.Settings[key].Value}");
+            }
+
+            // 'Folder/Web.config' settings will be merged
+            return View("~/Views/FolderConfig/many.cshtml", dict);
+        }
         private ActionResult Private()
+        {
+            return View("~/Views/Home/Ccs.cshtml", this.Dict());
+        }
+
+        private Dictionary<string, string> Dict()
         {
             Dictionary<string, string> dict = new Dictionary<string, string>();
             foreach (string key in WebConfigurationManager.AppSettings.Keys)
             {
-                dict.Add(key, $"{key} {WebConfigurationManager.AppSettings[key]}");
+                dict.Add(key, $"{key} {WebConfigurationManager.AppSettings[key]}"); //Note: WebConfigurationManager!
             }
 
-            return View("~/Views/Home/Ccs.cshtml", dict);
+            return dict;
         }
     }
 }
