9d23e780162ba8cb55fe2a7852d220237bb9f57b
Anton Lygin <lygin@live.com>
2016-11-03 00:28:14 +0300

    v0.014 - elements;

---
+++ ConfigMvc/Controllers/ElementsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace ConfigMvc.Controllers
+{
+    public class ElementsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.Private(); // Belarus
+        }
+
+        public ActionResult Another()
+        {
+            return this.Private(); // UK
+        }
+
+        private ActionResult Private()
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            foreach (string key in WebConfigurationManager.AppSettings.Keys)
+            {
+                dict.Add(key, $"{key} {WebConfigurationManager.AppSettings[key]}");
+            }
+
+            return View("~/Views/Home/Ccs.cshtml", dict);
+        }
+    }
+}
